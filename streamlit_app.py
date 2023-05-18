343eec4bc179486c31c096364e0551ce6d0d9515
Sadie Freisthler <72704819+sfreisthler@users.noreply.github.com>
2023-05-17 22:20:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ SHEET_ID = st.secrets.sheet_id
 SHEET_NAME = "Sheet1"
 url = f'https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}'
 df = pd.read_csv(url)
-print(df.head())
+st.balloons()
 
 # ------------ SETTINGS -------------
 page_icon = ":spaghetti:"
