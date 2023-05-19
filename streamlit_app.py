17fa131c24e33987cd5b005d487c7e88d953b722
sfreisthler <sfreisthler@gmail.com>
2023-05-19 01:46:19 -0400

    Added main function/cleaned up code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,3 @@
-from datetime import date
 import folium
 import requests
 
@@ -8,42 +7,6 @@ from streamlit_folium import st_folium
 
 import gspread
 import pandas as pd
-
-# --- LOAD SECRETS ---
-type = st.secrets.type
-project_id=st.secrets.project_id
-private_key_id=st.secrets.private_key_id
-private_key=st.secrets.private_key
-client_email=st.secrets.client_email
-client_id=st.secrets.client_id
-auth_uri=st.secrets.auth_uri
-token_uri=st.secrets.token_uri
-auth_provider=st.secrets.auth_provider_x509_cert_url
-client=st.secrets.client_x509_cert_url
-universe_domain=st.secrets.universe_domain
-
-creds = {
-    "type":type,
-    "project_id":project_id,
-    "private_key_id":private_key_id,
-    "private_key":private_key,
-    "client_email":client_email,
-    "client_id":client_id,
-    "auth_uri":auth_uri,
-    "token_uri":token_uri,
-    "auth_provider_x509_cert_url":auth_provider,
-    "client_x509_cert_url":client,
-    "universe_domain":universe_domain
-}
-
-# ------------ SETTINGS -------------
-page_icon = ":spaghetti:"
-page_title = "Italian Eats NYC"
-layout = "centered"
-#------------------------------------
-
-st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
-st.title(page_title + " " + page_icon)
 
 @st.cache_resource
 def load_sheet(creds):
@@ -64,77 +27,119 @@ def query_address(address):
         result = response.json()
     return result
 
-# --- Load Spreadsheet ---
-ws = load_sheet(creds)
-data = ws.get_all_values()
-headers = data.pop(0)
-df = pd.DataFrame(data, columns=headers)
+def hide_style():
+    hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+    st.markdown(hide_st_style, unsafe_allow_html=True)
 
+def load_secrets():
+    type = st.secrets.type
+    project_id=st.secrets.project_id
+    private_key_id=st.secrets.private_key_id
+    private_key=st.secrets.private_key
+    client_email=st.secrets.client_email
+    client_id=st.secrets.client_id
+    auth_uri=st.secrets.auth_uri
+    token_uri=st.secrets.token_uri
+    auth_provider=st.secrets.auth_provider_x509_cert_url
+    client=st.secrets.client_x509_cert_url
+    universe_domain=st.secrets.universe_domain
 
-completion_date = date.today()
+    creds = {
+    "type":type,
+    "project_id":project_id,
+    "private_key_id":private_key_id,
+    "private_key":private_key,
+    "client_email":client_email,
+    "client_id":client_id,
+    "auth_uri":auth_uri,
+    "token_uri":token_uri,
+    "auth_provider_x509_cert_url":auth_provider,
+    "client_x509_cert_url":client,
+    "universe_domain":universe_domain
+    }
+    return creds
 
-# --- HIDE STREAMLIT STYLE ---
-hide_st_style = """
-        <style>
-        #MainMenu {visibility: hidden;}
-        footer {visibility: hidden;}
-        header {visibility: hidden;}
-        </style>
-        """
-st.markdown(hide_st_style, unsafe_allow_html=True)
+def main():
+    # ------------ SETTINGS -------------
+    page_icon = ":spaghetti:"
+    page_title = "Italian Eats NYC"
+    layout = "centered"
+    #------------------------------------
+    st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
+    st.title(page_title + " " + page_icon)
 
-# --- NAVIGATION MENU ---
-selected = option_menu(
-    menu_title=None,
-    options=["Data Entry", "Data Visualization", "Data"],
-    icons=["pencil-fill", "bar-chart-fill","list-ol"],
-    orientation="horizontal",
-)
+    # --- HIDE STREAMLIT STYLE ---
+    hide_style()
 
-# --- INPUT & SAVE RESTAURANTS
-if selected == "Data Entry":
-    st.header("Add/Update Restaurant Entry")
-    with st.form("entry_form", clear_on_submit=True):
-        name=st.text_input("Restaurant Name:")
-        address=st.text_input("Address:")
-        service=st.slider("Service:",0.0,5.0,2.5,0.1)
-        food=st.slider("Food:",0.0,5.0,2.5,0.1)
-        ambience=st.slider("Ambience:",0.0,5.0,2.5,0.1)
-        travel_time=st.number_input("Travel Time (min):",0,120,0,1)
-        comment=st.text_area("Comments:")
+    # --- LOAD SECRETS ---
+    creds = load_secrets()
+
+    # --- Load Spreadsheet ---
+    ws = load_sheet(creds)
+    data = ws.get_all_values()
+    headers = data.pop(0)
+    df = pd.DataFrame(data, columns=headers)
+
+    # --- NAVIGATION MENU ---
+    selected = option_menu(
+        menu_title=None,
+        options=["Data Entry", "Data Visualization", "Data"],
+        icons=["pencil-fill", "bar-chart-fill","list-ol"],
+        orientation="horizontal",
+    )
+    # --- INPUT & SAVE RESTAURANTS
+    if selected == "Data Entry":
+        st.header("Add/Update Restaurant Entry")
+        with st.form("entry_form", clear_on_submit=True):
+            name=st.text_input("Restaurant Name:")
+            address=st.text_input("Address:")
+            service=st.slider("Service:",0.0,5.0,2.5,0.1)
+            food=st.slider("Food:",0.0,5.0,2.5,0.1)
+            ambience=st.slider("Ambience:",0.0,5.0,2.5,0.1)
+            travel_time=st.number_input("Travel Time (min):",0,120,0,1)
+            comment=st.text_area("Comments:")
+            
+            # --- Calculate Overall Rating ---
+            service_int=float(service)
+            food_int=float(food)
+            ambience_int=float(ambience)
+            overall = (service_int*25 + food_int*50 + ambience_int*25) / 100
+
+            # --- Get Coordinates ---
+            json_list = query_address(address)
+            json_dict = json_list[0]
+            lat = json_dict.get('lat')
+            lon = json_dict.get('lon')
+
         
-        # --- Calculate Overall Rating ---
-        service_int=float(service)
-        food_int=float(food)
-        ambience_int=float(ambience)
-        overall = (service_int*25 + food_int*50 + ambience_int*25) / 100
+            submitted = st.form_submit_button("Save Data")
+            if submitted:
+                data = [name,address,overall,service,food,ambience,travel_time,comment,lat,lon]
+                ws.append_row(data)
+                st.success("Data saved!")
+                st.balloons()
+    elif selected == "Data Visualization":
+        m = folium.Map(location=[40.7826, -73.9656], zoom_start=13)
+        for restaurant in range(0,len(df)):
+            folium.Marker (
+                location=[df.iloc[restaurant]['lat'], df.iloc[restaurant]['lon']],
+                popup=df.iloc[restaurant]['Name'],
+                icon=folium.Icon(color="red"),
+            ).add_to(m)
+        folium.TileLayer('stamentoner').add_to(m)
+        st_data = st_folium(m, width=725)
+    elif selected == "Data":
+        df1 = df.drop(['lat','lon'], axis=1)
+        st.dataframe(df1)
 
-        # --- Get Coordinates ---
-        json_list = query_address(address)
-        json_dict = json_list[0]
-        lat = json_dict.get('lat')
-        lon = json_dict.get('lon')
-
-    
-        submitted = st.form_submit_button("Save Data")
-        if submitted:
-            data = [name,address,overall,service,food,ambience,travel_time,comment,lat,lon]
-            ws.append_row(data)
-            st.success("Data saved!")
-            st.balloons()
-elif selected == "Data Visualization":
-    m = folium.Map(location=[40.7826, -73.9656], zoom_start=13)
-    for restaurant in range(0,len(df)):
-        folium.Marker (
-            location=[df.iloc[restaurant]['lat'], df.iloc[restaurant]['lon']],
-            popup=df.iloc[restaurant]['Name'],
-            icon=folium.Icon(color="red"),
-        ).add_to(m)
-    folium.TileLayer('stamentoner').add_to(m)
-    st_data = st_folium(m, width=725)
-elif selected == "Data":
-    df1 = df.drop(['lat','lon'], axis=1)
-    st.dataframe(df1)
+if __name__=="__main__":
+    main()
     
 
 
